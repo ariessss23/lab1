1c6b243f922e5223e7f7b3c534d22a1399032192
Danil <loshakovdanil7@yandex.ru>
2026-09-14 22:34:11 +0300

    hello

---
+++ hello.py
@@ -0,0 +1,7 @@
+# ВЫВОД ПЛАТФОРМЫ, ВЕРСИИ ПИТОНА И ПОЛЬЗОВАТЕЛЯ
+import platform
+import getpass
+print('HELLO, DANIL LOSHAKOV')
+print(platform.python_version())
+print(getpass.getuser())
+print(platform.system())
