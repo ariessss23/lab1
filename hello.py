2d8a855f0c0e03a94ce57268c1065f0a63c4cb2e
Danil <loshakovdanil7@yandex.ru>
2026-09-14 22:43:37 +0300

    Add current working directory output

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 # ВЫВОД ПЛАТФОРМЫ, ВЕРСИИ ПИТОНА И ПОЛЬЗОВАТЕЛЯ
 import platform
 import getpass
+import os
+
 print('HELLO, DANIL LOSHAKOV')
 print(platform.python_version())
 print(getpass.getuser())
 print(platform.system())
+print(os.getcwd())
